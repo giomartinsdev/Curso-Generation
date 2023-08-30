110b587234e9a8adf15a4bf7fe072f557777ba2c
giomartinsdev <giovannidealmeidamartins@gmail.com>
2023-08-30 13:25:21 -0300

    02 - Variaveis

---
+++ java/src/Variaveis.java
@@ -0,0 +1,25 @@
+public class Variaveis {
+    public static void main(String[] args){
+        double salario = 2521.99;
+        final int nasc = 28012005;
+        short cep = 23573;
+        long celular = 980957143;
+        float altura = 1.65F;
+        boolean admin = true;
+        byte id = 1;
+        char letraInicial = 'G';
+        String nome = "Giovani";
+
+        System.out.println("salario - " + salario);
+        System.out.println("nasc - " + nasc);
+        System.out.println("cep - " + cep);
+        System.out.println("celular - " + celular);
+        System.out.println("altura - " + altura);
+        System.out.println("admin - " + admin);
+        System.out.println("id - " + id);
+        System.out.println("letraInicial - " + letraInicial);
+        System.out.println("nome - " + nome);
+
+
+    }
+}
