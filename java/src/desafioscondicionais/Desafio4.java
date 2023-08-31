40e5b10678a6f9d6d92d230a7da6da38cee710a1
giomartinsdev <giovannidealmeidamartins@gmail.com>
2023-08-31 14:11:27 -0300

    05 - desafio de condicionais d4 comentado

--- java/src/desafioscondicionais/Desafio4.java
+++ java/src/desafioscondicionais/Desafio4.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Desafio4 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        //por boas praticas, todos os inputs devem ser minusculos e sem acentucao
-        System.out.println("por boas praticas, todos os inputs devem ser minusculos e sem acentucao\n");
+        // Introdução e solicitação de entrada do usuário
+        // Por boas praticas, todos os inputs devem ser minusculos e sem acentuacao
+        System.out.println("por boas praticas, todos os inputs devem ser minusculos e sem acentuacao\n");
         System.out.println("digite se é invertebrado ou vertebrado: \n");
         String vertebraeOfAnimal = reader.next();
 
@@ -16,29 +17,32 @@ public class Desafio4 {
         System.out.println("digite se o animal é carnivero, onivoro, herbivoro ou hemafago: \n");
         String eatOfAnimal = reader.next();
 
-
+        // Começa a estrutura de seleção usando switch
         switch (vertebraeOfAnimal) {
             case "vertebrado":
+                // Caso o animal seja vertebrado
                 switch (typeOfAnimal) {
                     case "ave":
+                        // Caso o animal seja ave
                         switch (eatOfAnimal) {
                             case "carnivoro":
-                                System.out.println("aguia");
+                                System.out.println("aguia"); // Saída para águia
                                 break;
                             case "onivoro":
-                                System.out.println("pomba");
+                                System.out.println("pomba"); // Saída para pomba
                                 break;
                             default:
                                 System.out.println("por favor insira carnivoro ou onivoro");
                         }
                         break;
                     case "mamifero":
+                        // Caso o animal seja mamífero
                         switch (eatOfAnimal) {
                             case "onivoro":
-                                System.out.println("homem");
+                                System.out.println("homem"); // Saída para homem
                                 break;
                             case "herbivoro":
-                                System.out.println("vaca");
+                                System.out.println("vaca"); // Saída para vaca
                                 break;
                             default:
                                 System.out.println("por favor insira onivoro ou herbivoro");
@@ -49,33 +53,36 @@ public class Desafio4 {
                 }
                 break;
             case "invertebrado":
+                // Caso o animal seja invertebrado
                 switch (typeOfAnimal) {
                     case "inseto":
+                        // Caso o animal seja inseto
                         switch (eatOfAnimal) {
                             case "hematofago":
-                                System.out.println("pulga");
+                                System.out.println("pulga"); // Saída para pulga
                                 break;
                             case "herbivoro":
-                                System.out.println("lagarta");
+                                System.out.println("lagarta"); // Saída para lagarta
                                 break;
                             default:
                                 System.out.println("por favor insira hematofago ou herbivoro");
                         }
                         break;
-                    case "mamifero":
+                    case "anelideo":
+                        // Caso o animal seja anelídeo (adicionado no comentário para correção)
                         switch (eatOfAnimal) {
                             case "hematofago":
-                                System.out.println("sanguessuga");
+                                System.out.println("sanguessuga"); // Saída para sanguessuga
                                 break;
                             case "onivoro":
-                                System.out.println("minhoca");
+                                System.out.println("minhoca"); // Saída para minhoca
                                 break;
                             default:
                                 System.out.println("por favor insira hematofago ou onivoro");
                         }
                         break;
                     default:
-                        System.out.println("por favor mamifero ou inseto");
+                        System.out.println("por favor inseto ou anelideo");
                 }
                 break;
             default:
