59d3cd7ca146cbe645d2948b435a7f9a720d24d2
giomartinsdev <giovannidealmeidamartins@gmail.com>
2023-09-11 14:54:10 -0300

    11.1 - atualizei o exercicio um

---
+++ java/src/desafioobjetos/desafio1/TestaCliente.java
@@ -0,0 +1,8 @@
+package desafioobjetos.desafio1;
+
+public class TestaCliente {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Giovanni","Martins",980957143,"Giovannimartins@gmail.com","Rio de Janeiro");
+        cliente.visualizar();
+    }
+}
--- java/src/desafioobjetos/desafio2/TestaFuncionario.java
+++ java/src/desafioobjetos/desafio2/TestaFuncionario.java
@@ -4,6 +4,5 @@ public class TestaFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario("Giovanni","Martins",980957143,"Dev",2543.23);
         funcionario.visualizar();
-
     }
 }
