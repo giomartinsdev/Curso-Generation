668b85b4ffdb5268ee1cb074a5d6df6ab5e1ffd9
giomartinsdev <giovannidealmeidamartins@gmail.com>
2023-09-11 11:40:27 -0300

    011 - desafios orientado a objetos

---
+++ java/src/desafioobjetos/desafio2/Funcionario.java
@@ -0,0 +1,68 @@
+package desafioobjetos.desafio2;
+
+public class Funcionario {
+
+    //------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------- ATRIBUTOS -----------------------------------------------------
+
+    private String nome;
+    private String sobrenome;
+    private Integer telefone;
+    private String cargo;
+    private Double salario;
+
+
+    //------------------------------------------------------------------------------------------------------------
+    //---------------------------------------METODOS SETTERS E GETTERS --------------------------------------------------
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public Integer getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public Double getSalario() {
+        return salario;
+    }
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    //------------------------------------------------------------------------------------------------------------
+    //---------------------------------------METODOS FUNCIONAIS --------------------------------------------------
+
+    public Funcionario(String nome, String sobrenome, Integer telefone, String cargo, Double salario) {
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setTelefone(telefone);
+        setCargo(cargo);
+        setSalario(salario);
+    }
+
+    public void visualizar(){
+        System.out.println("Nome do funcionario: "+getNome());
+        System.out.println("Sobrenome do funcionario: "+getSobrenome());
+        System.out.println("Telefone do funcionario: (21)"+getTelefone());
+        System.out.println("Cargo do funcionario: "+getCargo());
+        System.out.println("Salario do funcionario: "+getSalario());
+    }
+
+}
---
+++ java/src/desafioobjetos/desafio2/TestaFuncionario.java
@@ -0,0 +1,9 @@
+package desafioobjetos.desafio2;
+
+public class TestaFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Giovanni","Martins",980957143,"Dev",2543.23);
+        funcionario.visualizar();
+
+    }
+}
